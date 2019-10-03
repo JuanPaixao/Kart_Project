62e379720b7fef77a04ffe432836ef95b642d1b8
Juan Paixao <juanpaixao@live.com>
2019-10-02 23:37:40 -0300

    Implemented lv 2 drift boost, speedmeter UI

---
+++ KartProject/Assets/Scripts/UIManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text speedText;
+
+    public void SpeedUI(int speed)
+    {
+        speedText.text = speed.ToString();
+    }
+}
